1242c2c060ba2a1d3a679d0beb821bffe4ce8562
vinaygaja <56452269+vinaygaja@users.noreply.github.com>
2019-10-12 00:06:17 +0530

    Create S02Q01vehicle milege.py

---
+++ S02Q01vehicle milege.py
@@ -0,0 +1,17 @@
+#calculating milage of car
+
+from calci import sub,div
+
+sv=input("starting reading:")
+sv=int(sv)                             
+ev=input("ending reading:")
+ev=int(ev)
+fltr=input("fuel consume:")
+fltr=int(fltr)
+
+#from calci import sub
+vrun=sub(ev,sv)
+print(vrun)
+#from calci import div
+milage=div(vrun,fltr)
+print (milage)
