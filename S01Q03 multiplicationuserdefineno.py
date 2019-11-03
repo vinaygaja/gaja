183a8589fa5101b97f3755b9b40ec77ddcd50643
vinaygaja <56452269+vinaygaja@users.noreply.github.com>
2019-11-03 22:32:52 +0530

    Update S01Q03 multiplicationuserdefineno.py

--- S01Q03 multiplicationuserdefineno.py
+++ S01Q03 multiplicationuserdefineno.py
@@ -1,8 +1,18 @@
-#multiplication of user desired number
+#Question:
+
+##Modify program in S01Q02 to print the multiplication table 
+##           of any number desired by the user
+
+#Code:
+
+#multiplication table of user desired number
 c=1 
 a=input("number desired by user?")
 a=int(a)
-while c<11:
+#while c<11:
+while c!=0:
     table=a*c
     print(table)
     c+=1
+
+    
