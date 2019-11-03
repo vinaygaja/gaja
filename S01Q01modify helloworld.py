ccc1600c7eb1670f6822a41791ebb334d5054fa3
vinaygaja <56452269+vinaygaja@users.noreply.github.com>
2019-11-03 22:54:16 +0530

    Update S01Q01modify helloworld.py

--- S01Q01modify helloworld.py
+++ S01Q01modify helloworld.py
@@ -1,5 +1,20 @@
+## Modify the first “Hello World” program to prompt for user’s name 
+##           and then wish the user by saying Hello followed by his name
+##
+##           Example :
+##           If user's name is Sam, then expected output is :
+##           Hello Sam !!!
+
+#Code:
+
 #modify the first hello world
 
-
 user=input("enter the user:")
-print("hello",user)
+print("Hello",user,"!!!")
+
+
+#Getting output:
+
+## RESTART: C:\Users\Administrator\Desktop\pythonexercises\S01Q01modify helloworld.py.py 
+##enter the user:Sam
+##Hello Sam !!!
