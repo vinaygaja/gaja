4f9994060fb9b975fb113a94afab93ecc922b3fb
vinaygaja <56452269+vinaygaja@users.noreply.github.com>
2019-11-06 03:02:17 +0530

    Create S09Q01generatewebpage.py

---
+++ S09Q01generatewebpage.py
@@ -0,0 +1,29 @@
+##Question:
+##Write a program that generates a HTML webpage. 
+##Prompt the user for webpage title and webpage body contents. 
+##The webpage body should contain 
+## - one main header, 
+## - one sub header, and 
+## - at least 2 paragraphs.
+
+ #Code:
+#main starts
+title=input('webpage title:')
+#body of a page
+h1=input('header:')
+h2=input('subheader:')
+p1=input('paragraph:')
+p2=input('paragraph:')
+print('          ',title+'.html')
+
+print('                           ',h1)
+
+print('                           ',h2)
+
+print('',p1)
+
+
+
+
+
+print('',p2)
